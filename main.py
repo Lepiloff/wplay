1759161eae3272de883f61128357d62e0bfab649
Lepiloff <lepiloff82@gmail.com>
2023-01-17 17:48:57 +0100

    add https_url_for function

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ templates = Jinja2Templates(directory="templates")
 
 
 # standard url_for method generate http instead https links
-def https_url_for(request: Request, name: str, **path_params: Any) -> str:
+def https_url_for(context: dict, name: str, **path_params) -> str:
+    request = context["request"]
     http_url = request.url_for(name, **path_params)
-    # Replace 'http' with 'https'
     return http_url.replace("http", "https", 1)
 
 
-templates.env.globals["https_url_for"] = https_url_for
+templates.env.globals["url_for"] = https_url_for
 # TODO add status_code
 
 
