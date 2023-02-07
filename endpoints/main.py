bad39ae776abf3c69e2888f1b338de94012dc0c8
Lepiloff <lepiloff82@gmail.com>
2023-02-07 22:54:24 +0100

    minore

--- endpoints/main.py
+++ endpoints/main.py
@@ -1,4 +1,3 @@
-from typing import Any
 from fastapi import APIRouter, Request, Form, Depends
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
@@ -7,17 +6,8 @@ from services.user import UserService
 from models.users import users as User
 
 
-
-def https_url_for(request: Request, name: str, **path_params: Any) -> str:
-
-    http_url = request.url_for(name, **path_params)
-
-    # Replace 'http' with 'https'
-    return http_url.replace("http", "https", 1)
-
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-templates.env.globals["https_url_for"] = https_url_for
 
 
 @router.get('/', response_class=HTMLResponse)
