cd2be9a8ae84c5d047cc3acd08099f3f9af940ee
Lepiloff <lepiloff82@gmail.com>
2023-02-20 13:33:57 +0100

    add html

--- endpoints/events.py
+++ endpoints/events.py
@@ -11,20 +11,27 @@ templates = Jinja2Templates(directory="templates")
 # dont change method order
 @router.get('/all')
 async def _events():
-    ev = await events.get_all()
+    return templates.TemplateResponse('base.html',
+                                      context={'request': Request,
+                                      }
+                                      )
+#
+#
+# @router.post('/all')
+# async def event_create(request: Request, street: str = Form(...), house: str = Form(...),
+#                                     activity: str = Form(...), title: str = Form(...),
+#                                     content: str = Form(...)):
+#     event = await events.post(street, house, title, content, activity)
+#     # return templates.TemplateResponse('events.html', context={'request': request, 'result': event})
+#
+#
+# @router.get('/{pk}')
+# async def event(request: Request, pk: int):
+#     event = await events.get(pk)
+#     # return templates.TemplateResponse('events.html', context={'request': request, 'result': event})
 
 
-@router.post('/all')
-async def event_create(request: Request, street: str = Form(...), house: str = Form(...),
-                                    activity: str = Form(...), title: str = Form(...),
-                                    content: str = Form(...)):
-    event = await events.post(street, house, title, content, activity)
-    # return templates.TemplateResponse('events.html', context={'request': request, 'result': event})
-
-
-@router.get('/{pk}')
-async def event(request: Request, pk: int):
-    event = await events.get(pk)
-    # return templates.TemplateResponse('events.html', context={'request': request, 'result': event})
-
-
+# @router.get('/{pk}')
+# async def event(request: Request, pk: int):
+#     event = await events.get(pk)
+#     # return templates.TemplateResponse('events.html', context={'request': request, 'result': event})
